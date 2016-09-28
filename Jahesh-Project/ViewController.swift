caf157c090cc0b3b945284d7c3dade0b105b3e0f
Ali Soume'e <alidadar7676@gmail.com>
2016-09-28 10:33:45 +0330

    Initial Commit

---
+++ Jahesh-Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jahesh-Project
+//
+//  Created by Ali Soume`e on 7/7/1395 AP.
+//  Copyright © 1395 Ali-Soume. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
